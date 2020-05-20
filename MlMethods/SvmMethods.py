ac0e62131fd8b71210eda52716cbcf72c2eebd56
TahaBerkay <tahaberkayduman.tbd@gmail.com>
2020-05-20 22:56:39 +0300

    only rbf

--- MlMethods/SvmMethods.py
+++ MlMethods/SvmMethods.py
@@ -7,7 +7,7 @@ from MlMethods import Methods
 
 grid_params = {'C': [0.001, 0.01, 0.1, 1, 10, 100],
                'gamma': ['scale', 'auto'],
-               'kernel': ['linear', 'rbf']}
+               'kernel': ['rbf']}
 
 
 class SvmMethod(Methods.Method):
